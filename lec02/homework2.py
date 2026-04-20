4384b9d214805ec6cf364e874b5eb5d4bcfeefd9
rajeshthami62-pixel <rajeshthami62@gmail.com>
2026-04-20 10:58:11 +0900

    homework2.py
    updated

--- lec02/homework2.py
+++ lec02/homework2.py
@@ -12,14 +12,13 @@ def arithmetic(x, y):
     isinstance(y,str)   return x+y         return str(x)+y
     isinstance(y,float) return x*int(y)    return x*y
     """
-    def arithmetic(x, y):
-    if isinstance(y, str):
-        if isinstance(x, str):
+    if isinstance(x,str):
+        if isinstance(y,str):
             return x + y
         else: 
-            return str(x) + y
+            return strx*int(y)
     else:  
-        if isinstance(x, str):
-            return x * int(y)
+        if isinstance(y,str):
+            return str(x)+y
         else:
-            return x * y
+            return x*y
