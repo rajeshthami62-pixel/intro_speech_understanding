322fd048f09d6778f15abd7202a58a4fda3fabdb
rajeshthami62-pixel <rajeshthami62@gmail.com>
2026-05-11 10:28:13 +0900

    homework 3
    done

--- lec03/homework3.py
+++ lec03/homework3.py
@@ -14,5 +14,11 @@ def words2characters(words):
     example, if words==['hello', 1.234, True], then characters should be
     ['h', 'e', 'l', 'l', 'o', '1', '.', '2', '3', '4', 'T', 'r', 'u', 'e']
     """
-    return []
+    listofcharacters = []
+
+    for word in words:
+        for c in str(word):
+            listofcharacters.append(c)
+
+    return listofcharacters
 
