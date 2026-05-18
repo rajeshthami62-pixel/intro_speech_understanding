054ff02a99ea27f551adc2062e6d46ae1ae019c8
rajeshthami62-pixel <rajeshthami62@gmail.com>
2026-05-18 09:53:12 +0900

    homework 4
    updated

--- lec04/homework4.py
+++ lec04/homework4.py
@@ -12,7 +12,19 @@ def next_birthday(date, birthdays):
     birthday - the next day, after given date, on which somebody has a birthday
     list_of_names - list of all people with birthdays on that date
     '''
-    birthday = (1,1)
-    list_of_names = []
-    return birthday, list_of_names
-    
+    month = date[0]
+    day = date[1]
+
+    sorted_keys = sorted(birthdays.keys())
+
+    i = 0
+    while i < len(sorted_keys) and sorted_keys[i][0] < month:
+        i += 1
+
+    while (i < len(sorted_keys) and
+           sorted_keys[i][0] == month and
+           sorted_keys[i][1] <= day):
+        i += 1
+    if i == len(sorted_keys):
+        return sorted_keys[0], birthdays[sorted_keys[0]]
+    return sorted_keys[i], birthdays[sorted_keys[i]]
