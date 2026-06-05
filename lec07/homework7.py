306921be724732f082ff4c8b4b9226f9baead193
rajeshthami62-pixel <rajeshthami62@gmail.com>
2026-06-05 14:56:23 +0900

    homework 7
    update

--- lec07/homework7.py
+++ lec07/homework7.py
@@ -10,26 +10,38 @@ def major_chord(f, Fs):
 
     @return:
     x (array): a one-half-second waveform containing the chord
-    
+
     A major chord is three notes, played at the same time:
     (1) The root tone (f)
     (2) A major third, i.e., four semitones above f
     (3) A major fifth, i.e., seven semitones above f
     '''
-    raise RuntimeError("You need to write this part")
+    n = np.arange(int(Fs/2))
+
+    note1 = np.cos(2*np.pi*f*n/Fs)
+    note2 = np.cos(2*np.pi*f*(2**(4/12))*n/Fs)
+    note3 = np.cos(2*np.pi*f*(2**(7/12))*n/Fs)
+
+    return note1 + note2 + note3
 
 def dft_matrix(N):
     '''
     Create a DFT transform matrix, W, of size N.
-    
+
     @param:
     N (scalar): number of columns in the transform matrix
-    
+
     @result:
     W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
            W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
     '''
-    raise RuntimeError("You need to write this part")
+    W = np.zeros((N,N), dtype='complex')
+
+    for k in range(N):
+        for n in range(N):
+            W[k,n] = np.cos(2*np.pi*k*n/N) - 1j*np.sin(2*np.pi*k*n/N)
+
+    return W
 
 def spectral_analysis(x, Fs):
     '''
@@ -43,4 +55,19 @@ def spectral_analysis(x, Fs):
     f1, f2, f3: The three loudest frequencies (in Hertz)
       These should be sorted so f1 < f2 < f3.
     '''
-    raise RuntimeError("You need to write this part")
+    N = len(x)
+
+    W = dft_matrix(N)
+    X = np.abs(np.dot(W, x))
+
+    k1 = np.argmax(X[:int(N/2)])
+    X[k1] = 0
+
+    k2 = np.argmax(X[:int(N/2)])
+    X[k2] = 0
+
+    k3 = np.argmax(X[:int(N/2)])
+
+    f_sorted = np.array(sorted([k1, k2, k3])) * Fs / N
+
+    return f_sorted[0], f_sorted[1], f_sorted[2]
