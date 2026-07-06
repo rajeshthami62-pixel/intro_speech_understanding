61304401ce86b9d0d15cf505c5a87d1ffdb5d83c
rajeshthami62-pixel <rajeshthami62@gmail.com>
2026-07-06 09:08:11 +0900

    Homework 9
    update homework 9

--- lec09/homework9.py
+++ lec09/homework9.py
@@ -13,7 +13,28 @@ def VAD(waveform, Fs):
     segments (list of arrays) - list of the waveform segments where energy is 
        greater than 10% of maximum energy
     '''
-    raise RuntimeError("You need to change this part")
+    framelength = int(0.025 * Fs)
+    frameskip = int(0.010 * Fs)
+
+    frames = np.array([waveform[n:n+framelength]
+                       for n in range(0, len(waveform)-framelength+1, frameskip)])
+
+    energies = np.sum(frames**2, axis=1)
+    e = 0.1 * np.max(energies)
+
+    VAD = [1 if energies[m] > e else 0 for m in range(len(energies))]
+
+    framestarts = [m for m in range(1, len(energies))
+                   if VAD[m] == 1 and VAD[m-1] == 0]
+
+    frameends = [m for m in range(1, len(energies))
+                 if VAD[m] == 0 and VAD[m-1] == 1]
+
+    segments = [waveform[framestarts[k]*frameskip:frameends[k]*frameskip+framelength]
+                for k in range(min(len(framestarts), len(frameends)))]
+
+    return segments
+
 
 def segments_to_models(segments, Fs):
     '''
@@ -29,7 +50,27 @@ def segments_to_models(segments, Fs):
     @returns:
     models (list of arrays) - average log spectra of pre-emphasized waveform segments
     '''
-    raise RuntimeError("You need to change this part")
+    models = []
+
+    framelength = int(0.004 * Fs)
+    frameskip = int(0.002 * Fs)
+
+    for k in range(len(segments)):
+        waveform = segments[k]
+
+        waveform = np.append(waveform[0], waveform[1:] - 0.95 * waveform[:-1])
+
+        frames = np.array([waveform[n:n+framelength]
+                           for n in range(0, len(waveform)-framelength+1, frameskip)])
+
+        mstft = np.abs(np.fft.fft(frames, axis=1))
+
+        lsgram = 20 * np.log10(np.maximum(mstft, 0.001 * np.amax(mstft)))
+
+        models.append(np.mean(lsgram[:, :int(framelength//2)], axis=0))
+
+    return models
+
 
 def recognize_speech(testspeech, Fs, models, labels):
     '''
@@ -47,6 +88,31 @@ def recognize_speech(testspeech, Fs, models, labels):
     sims (Y-by-K array) - cosine similarity of each model to each test segment
     test_outputs (list of strings) - recognized label of each test segment
     '''
-    raise RuntimeError("You need to change this part")
+    segments = VAD(testspeech, Fs)
 
+    sims = np.zeros((len(models), len(segments)))
+    test_outputs = []
 
+    framelength = int(0.004 * Fs)
+    frameskip = int(0.002 * Fs)
+
+    for k in range(len(segments)):
+        x = segments[k]
+
+        frames = np.array([x[m:m+framelength]
+                           for m in range(0, len(x)-framelength+1, frameskip)])
+
+        mstft = np.abs(np.fft.fft(frames, axis=1))
+
+        sgram = 20 * np.log10(np.maximum(mstft, 0.001 * np.amax(mstft)))
+
+        spectrum = np.mean(sgram[:, :int(framelength//2)], axis=0)
+
+        for y in range(len(models)):
+            sims[y, k] = np.dot(spectrum, models[y]) / (
+                np.linalg.norm(spectrum) * np.linalg.norm(models[y])
+            )
+
+        test_outputs.append(labels[np.argmax(sims[:, k])])
+
+    return sims, test_outputs
